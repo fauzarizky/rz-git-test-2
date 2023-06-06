ddb98f5744ff1a27d50393d5ce0bfacb55b8973f
fauza rizky <fauzasmg@gmail.com>
2023-06-06 09:25:45 +0700

    menambahkan list

--- index.php
+++ index.php
@@ -9,5 +9,9 @@
 <body>
     <h1>Hello World!</h1>
     <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. In provident quo, dolore nulla animi id blanditiis maxime labore quas beatae accusamus tempora earum architecto ab aliquam eos error distinctio molestias!</p>
+    <ul>
+        <li>satu</li>
+        <li>dua</li>
+    </ul>
 </body>
 </html>
