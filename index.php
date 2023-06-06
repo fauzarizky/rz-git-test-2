c06e05712a2524cd417dfffd9ce4d98426189cb4
fauza rizky <fauzasmg@gmail.com>
2023-06-06 09:16:23 +0700

    menambahkan paragraf

--- index.php
+++ index.php
@@ -8,5 +8,6 @@
 </head>
 <body>
     <h1>Hello World!</h1>
+    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. In provident quo, dolore nulla animi id blanditiis maxime labore quas beatae accusamus tempora earum architecto ab aliquam eos error distinctio molestias!</p>
 </body>
 </html>
